909b3df0d31377862d6af8816061ac6e98eb55b5
itsknk <kishorekaranam99@gmail.com>
2024-06-27 19:00:41 -0700

    constants for the lexer and the parser files

---
+++ helper.py
@@ -0,0 +1,7 @@
+true_length = len('true')
+false_length = len('false')
+null_length = len('null')
+json_quote = '"'
+# Whitespace characters that should be ignored during lexing.
+json_ignore = ['\b', '\n', '\t', ' ', '\r']
+json_syntax = [':', ',', '{', '}', '[', ']']
