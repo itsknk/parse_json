72eb77cb1e0cbd4810e69af2efb52496e4d49130
itsknk <kishorekaranam99@gmail.com>
2024-06-27 19:01:51 -0700

    Move the constants to a standalone file

--- lex.py
+++ lex.py
@@ -1,10 +1,4 @@
-true_length = len('true')
-false_length = len('false')
-null_length = len('null')
-json_quote = '"'
-# Whitespace characters that should be ignored during lexing.
-json_ignore = ['\b', '\n', '\t', ' ', '\r']
-json_syntax = [':', ',', '{', '}', '[', ']']
+from helper import *
 
 def lex_string(string):
     """
